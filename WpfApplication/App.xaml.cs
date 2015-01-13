9e661bb185ebfabe13dbf8292ada7a0f9474305e
cyranothedaft <cyranothedev@gmail.com>
2015-01-12 20:25:37 -0700

    new WPF project

---
+++ WpfApplication/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApplication.ViewModels;
+
+
+
+namespace WpfApplication {
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application {
+      protected override void OnStartup(StartupEventArgs e) {
+         base.OnStartup(e);
+
+         MainViewModel mainViewModel = new MainViewModel();
+         mainViewModel.CheckBoxContent = "Check Me";
+
+         MainWindow mainWindow = new MainWindow();
+         mainWindow.ViewModel = mainViewModel;
+         mainWindow.Show();
+      }
+   }
+}
---
+++ WpfApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication.ViewModels {
+   internal sealed class MainViewModel {
+      public string CheckBoxContent { get; set; }
+   }
+}
